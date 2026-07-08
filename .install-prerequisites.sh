275a2d5034fa8ddd953280461d85143d3026a17b
Marco Re <mre@adobe.com>
2026-07-08 19:25:28 +0200

    Define brew and mac app list + start setup script and prereq

---
+++ .install-prerequisites.sh
@@ -0,0 +1,27 @@
+!/bin/bash
+set -ex
+
+case "$(uname -s)" in
+Darwin)
+    if type bw >/dev/null 2>&1; then
+        echo "Bitwarden CLI is already installed"
+    else
+        brew install pnpm
+            
+    fi
+
+    read -p "Please open Bitwarden, log into all accounts and set under Settings>CLI activate Integrate with Bitwarden CLI. Press any key to continue." -n 1 -r
+    echo
+    ;;
+Linux)
+    echo "Linux detected — skipping Homebrew"
+    if ! type bw >/dev/null 2>&1; then
+        curl -fsSL https://get.pnpm.io/install.sh | sh -
+        npm install -g @bitwarden/cli
+    fi
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
