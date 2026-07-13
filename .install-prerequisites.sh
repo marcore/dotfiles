8722f33603ddd2226268f29adb820da52c3caf43
Marco Re <mre@adobe.com>
2026-07-13 12:18:41 +0200

    Simplify with brew bitwarden, then it will be removed as bring node with conflicts with mise

--- .install-prerequisites.sh
+++ .install-prerequisites.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 set -ex
 
-export PNPM_HOME="$HOME/.local/share/pnpm" # pnpm's global bin dir must be in PATH before `pnpm install -g` will run
-export PATH="$PNPM_HOME/bin:$PATH"
-
 case "$(uname -s)" in
 Darwin)
     if type bw >/dev/null 2>&1; then
         echo "Bitwarden CLI is already installed"
     else
-        brew install pnpm mise
-        mise trust $HOME/.config/mise/config.toml && mise install node --verbose # Node is needed for pnpm
-        eval "$(mise env -s bash)" # put mise-installed node on PATH for this script
-        pnpm install -g buffer @bitwarden/cli
+        brew install bitwarden-cli
     fi
 
     if [ "${CI:-}" != "true" ]; then
@@ -23,11 +17,7 @@ Darwin)
 Linux)
     echo "Linux detected — skipping Homebrew"
     if ! type bw >/dev/null 2>&1; then
-        curl -fsSL https://get.pnpm.io/install.sh | sh -
-        sudo apt-get update && sudo apt-get install -y mise
-        mise trust $HOME/.config/mise/config.toml && mise install node --verbose # Node is needed for pnpm
-        eval "$(mise env -s bash)" # put mise-installed node on PATH for this script
-        pnpm install -g buffer @bitwarden/cli
+        sudo apt-get update && sudo snap install bw
     fi
     ;;
 *)
